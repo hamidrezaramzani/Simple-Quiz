3642493a79b2b04ae07ac713466277c5293bee6a
Hamidreza Ramzani <hrra13801396h@gmail.com>
2020-01-08 14:47:09 +0330

    Add json file for get questions

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
-//H ypothetical data
-const data = [
-  {
-    question: "what is your name?",
-    answers: [ 
-      "ok" , "no" , "by" , "hello"
-    ],
-    level: "hard", 
-    
-  }
-];
-
-
-
-const xmlHttp = new XMLHttpRequest();
 document.getElementById("startQuizBtn").addEventListener("click", function(e) {
   e.preventDefault();
   const langInput = document.getElementById("lang");
@@ -21,18 +6,9 @@ document.getElementById("startQuizBtn").addEventListener("click", function(e) {
     lang: langInput.value,
     level: levelInput.value
   };
-  changePage(data);
+  startGame(data);
 });
 
-async function changePage(data) {
-  xmlHttp.onreadystatechange = () => {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      console.log(xmlHttp.response);
-    } else {
-      alert("error!");
-    }
-  };
-  xmlHttp.open("POST", "localhost:3000/api/questions");
-  xmlHttp.setRequestHeader("content-type", "application/json");
-  xmlHttp.send(data);
-};
+function startGame(data) {
+ 
+}
